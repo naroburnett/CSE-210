c4f1e3f5d288beb0e75b221726bf65ba1cfd0bd8
Nathan Burnett <bur19052@byui.edu>
2022-04-05 17:18:11 -0600

    Update 1

---
+++ Final/Digital_Racers/digital_racers/game/actors/player.py
@@ -0,0 +1,13 @@
+from constants 
+
+class Player(vehicle):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
